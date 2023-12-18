ed04005fbf9f02ce0d9e40bc658059c2934328b4
ArthurDMVM <115365021+ArthurDMVM@users.noreply.github.com>
2023-12-18 19:08:37 -0300

    New Mobile Style - Toolbar/Content

--- src/script.js
+++ src/script.js
@@ -3,6 +3,6 @@ import './style.scss';
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("menu-toggle").addEventListener("click", function () {
         document.getElementById("menu").classList.toggle("active");
-        this.classList.toggle("hidden");
+        // this.classList.toggle("hidden");
     });
 });
